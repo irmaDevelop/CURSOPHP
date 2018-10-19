95edf65ac21566a9f5227e803bc3ad36a729f878
IrmaJulia <irmajuliadavila@icloud.com>
2018-10-19 10:56:51 -0500

    Commit antes de hostvirtual

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,7 +2,14 @@
 namespace App\Models;
 
 //require_once 'BaseElement.php';
+require_once 'Printable.php';
+use Illuminate\Database\Eloquent\Model;
 
-class Project extends BaseElement{
+class Project extends Model{
+    protected $table = 'projects'; //la copio de la documentacion de eloquent
 
+
+    public function getDurationAsString(){
+		return '';
+	}
 }
